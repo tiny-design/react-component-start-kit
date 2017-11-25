dc30730339b95839851264a02ccd4705dc62216b
Di Wang <wangd7@student.unimelb.edu.au>
2017-11-25 23:47:13 +1100

    production webpack && deploy

--- site/webpack.config.prod.js
+++ site/webpack.config.prod.js
@@ -1,15 +1,53 @@
-module.exports = {
-    devtool: 'eval-source-map',
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-    entry:  __dirname + "/app/index.js",
+module.exports = {
+    entry: [
+        path.resolve(__dirname, 'app/index.js'),
+    ],
     output: {
-        path: __dirname + "/public",
+        path: path.resolve(__dirname, '../build', 'site'),
         filename: "bundle.js"
     },
-
-    devServer: {
-        contentBase: "./public",
-        historyApiFallback: true,
-        inline: true
-    }
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(ttf|eot|svg|woff|woff2)(\?.+)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(jpe?g|png|gif)(\?.+)?$/,
+                loader: 'url-loader'
+            },
+            {
+                test: /\.md$/,
+                loader: 'raw-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html')
+        })
+    ]
 };
